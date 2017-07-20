961bb515bdf8ba4303e2cb799af9cb895c31a1dd
initial commit <git@lowi.org>
2017-07-20 15:17:50 +0200

    integrated typeform. easy as…

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,17 @@
 import jQuery from 'jquery'
 global.jQuery = jQuery
 
+function _adjustTypeformSize() {
+  let w = $(window).height(),
+      m = $('.masthead').height(),
+      iframe = $('.typeform-widget').css('height', `${w-m}px`)
+
+  console.log('_adjustTypeformSize', w, m)
+
+}
 
 $(document)
-  .ready(function () { console.log('hello')})
+  .ready(function () { 
+    console.log('hallo')
+    _adjustTypeformSize()
+    $(window).on('resize', _adjustTypeformSize)})
